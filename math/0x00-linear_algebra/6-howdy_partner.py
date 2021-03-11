0479600c3849bb340840a1eee525e5e742a34193
eyadev <46867140+eyadev@users.noreply.github.com>
2021-03-11 09:19:19 +0100

    Update 6-howdy_partner.py

--- math/0x00-linear_algebra/6-howdy_partner.py
+++ math/0x00-linear_algebra/6-howdy_partner.py
@@ -1,4 +1,3 @@
-alexa@ubuntu-xenial:0x00-linear_algebra$ cat 6-main.py 
 #!/usr/bin/env python3
 
 cat_arrays = __import__('6-howdy_partner').cat_arrays
@@ -12,8 +11,3 @@ arr2 = [6, 7, 8]
 print(cat_arrays(arr1, arr2))
 print(arr1)
 print(arr2)
-alexa@ubuntu-xenial:0x00-linear_algebra$ ./6-main.py 
-[1, 2, 3, 4, 5, 6, 7, 8]
-[1, 2, 3, 4, 5]
-[6, 7, 8]
-alexa@ubuntu-xenial:0x00-linear_algebra$ 
