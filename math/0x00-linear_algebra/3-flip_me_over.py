7613856ce40f60524939bc6daa59561a666e358c
eyadev <46867140+eyadev@users.noreply.github.com>
2021-03-11 21:21:03 +0100

    Update 3-flip_me_over.py

--- math/0x00-linear_algebra/3-flip_me_over.py
+++ math/0x00-linear_algebra/3-flip_me_over.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
+
 def matrix_transpose(matrix):
-    new_matrix = []
-    for i in range(len(matrix[0])):
-        N_row = []
-        for j in range(len(matrix)):
-            N_row.append(matrix[j][i])
-        new_matrix.append(N_row)
-    return new_matrix
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            result[j][i] = matrix[i][j]
+            return result
