4c6fdfcf68fe3a20b458c21b499d547fc711b5b6
eyadev <46867140+eyadev@users.noreply.github.com>
2021-03-11 21:53:58 +0100

    Update 7-gettin_cozy.py

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-cat_matrices2D = __import__('7-gettin_cozy').cat_matrices2D
 import numpy as np 
 
 def cat_matrices2D(mat1, mat2, axis=0):
@@ -12,15 +11,4 @@ def cat_matrices2D(mat1, mat2, axis=0):
         return new_mat.tolist()
     else:
         return None
-mat1 = [[1, 2], [3, 4]]
-mat2 = [[5, 6]]
-mat3 = [[7], [8]]
-mat4 = cat_matrices2D(mat1, mat2)
-mat5 = cat_matrices2D(mat1, mat3, axis=1)
-print(mat4)
-print(mat5)
-mat1[0] = [9, 10]
-mat1[1].append(5)
-print(mat1)
-print(mat4)
-print(mat5)
+
