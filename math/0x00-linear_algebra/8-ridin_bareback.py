f0496089711091b127c9021f0de009ed369bc547
eyadev <46867140+eyadev@users.noreply.github.com>
2021-03-11 09:20:54 +0100

    Update 8-ridin_bareback.py

--- math/0x00-linear_algebra/8-ridin_bareback.py
+++ math/0x00-linear_algebra/8-ridin_bareback.py
@@ -1,4 +1,3 @@
-alexa@ubuntu-xenial:0x00-linear_algebra$ cat 8-main.py
 #!/usr/bin/env python3
 
 mat_mul = __import__('8-ridin_bareback').mat_mul
@@ -15,6 +14,3 @@ mat1 = [[1, 2],
 mat2 = [[1, 2, 3, 4],
         [5, 6, 7, 8]]
 print(mat_mul(mat1, mat2))
-alexa@ubuntu-xenial:0x00-linear_algebra$ ./8-main.py
-[[11, 14, 17, 20], [23, 30, 37, 44], [35, 46, 57, 68]]
-alexa@ubuntu-xenial:0x00-linear_algebra$ 
