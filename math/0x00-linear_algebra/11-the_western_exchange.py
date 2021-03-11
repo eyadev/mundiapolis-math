12ff6cf282471dd8e39ff210af2ea5724f425b13
eyadev <46867140+eyadev@users.noreply.github.com>
2021-03-11 09:15:42 +0100

    Update 11-the_western_exchange.py

--- math/0x00-linear_algebra/11-the_western_exchange.py
+++ math/0x00-linear_algebra/11-the_western_exchange.py
@@ -1,4 +1,3 @@
-alexa@ubuntu-xenial:0x00-linear_algebra$ cat 11-main.py 
 #!/usr/bin/env python3
 
 import numpy as np
@@ -16,28 +15,3 @@ print(np_transpose(mat2))
 print(mat2)
 print(np_transpose(mat3))
 print(mat3)
-alexa@ubuntu-xenial:0x00-linear_algebra$ ./11-main.py 
-[1 2 3 4 5 6]
-[1 2 3 4 5 6]
-[]
-[]
-[[[ 1 11]
-  [ 6 16]]
-
- [[ 2 12]
-  [ 7 17]]
-
- [[ 3 13]
-  [ 8 18]]
-
- [[ 4 14]
-  [ 9 19]]
-
- [[ 5 15]
-  [10 20]]]
-[[[ 1  2  3  4  5]
-  [ 6  7  8  9 10]]
-
- [[11 12 13 14 15]
-  [16 17 18 19 20]]]
-alexa@ubuntu-xenial:0x00-linear_algebra$ 
