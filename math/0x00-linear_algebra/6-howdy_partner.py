81974a21d964b1615e667ecab5b55dcc9558a32b
eyadev <46867140+eyadev@users.noreply.github.com>
2021-03-11 21:53:11 +0100

    Update 6-howdy_partner.py

--- math/0x00-linear_algebra/6-howdy_partner.py
+++ math/0x00-linear_algebra/6-howdy_partner.py
@@ -1,13 +1,6 @@
 #!/usr/bin/env python3
 
-cat_arrays = __import__('6-howdy_partner').cat_arrays
 import numpy as np
 def cat_arrays(arr1, arr2):
     arr = np.concatenate((arr1, arr2))
     return arr.tolist()
-
-arr1 = [1, 2, 3, 4, 5]
-arr2 = [6, 7, 8]
-print(cat_arrays(arr1, arr2))
-print(arr1)
-print(arr2)
