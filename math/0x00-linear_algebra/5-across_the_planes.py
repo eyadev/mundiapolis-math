d8e9e0fa069fbb49ac2fa4611461bf38ca54ed3c
eyadev <46867140+eyadev@users.noreply.github.com>
2021-03-11 09:18:53 +0100

    Update 5-across_the_planes.py

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -1,4 +1,3 @@
-alexa@ubuntu-xenial:0x00-linear_algebra$ cat 5-main.py 
 #!/usr/bin/env python3
 
 add_matrices2D = __import__('5-across_the_planes').add_matrices2D
@@ -18,9 +17,3 @@ print(add_matrices2D(mat1, mat2))
 print(mat1)
 print(mat2)
 print(add_matrices2D(mat1, [[1, 2, 3], [4, 5, 6]]))
-alexa@ubuntu-xenial:0x00-linear_algebra$ ./5-main.py 
-[[6, 8], [10, 12]]
-[[1, 2], [3, 4]]
-[[5, 6], [7, 8]]
-None
-alexa@ubuntu-xenial:0x00-linear_algebra$ 
