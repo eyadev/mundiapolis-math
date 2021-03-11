c6ea84abb568e703e1955b4666d168d6a92179fb
eyadev <46867140+eyadev@users.noreply.github.com>
2021-03-11 21:50:51 +0100

    Update 5-across_the_planes.py

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-add_matrices2D = __import__('5-across_the_planes').add_matrices2D
 import numpy as np
 def add_matrices2D(mat1, mat2):
     mat1 = np.matrix(mat1)
@@ -11,9 +10,3 @@ def add_matrices2D(mat1, mat2):
     else:  
         return "None"
 
-mat1 = [[1, 2], [3, 4]]
-mat2 = [[5, 6], [7, 8]]
-print(add_matrices2D(mat1, mat2))
-print(mat1)
-print(mat2)
-print(add_matrices2D(mat1, [[1, 2, 3], [4, 5, 6]]))
