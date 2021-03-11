d627c0cd1666411ece5b5a0e5364ba364c7a626b
eyadev <46867140+eyadev@users.noreply.github.com>
2021-03-11 09:16:54 +0100

    Update 12-bracin_the_elements.py

--- math/0x00-linear_algebra/12-bracin_the_elements.py
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -1,8 +1,7 @@
-alexa@ubuntu-xenial:0x00-linear_algebra$ cat 12-main.py 
 # !/usr/bin/env python3
 
-import numpy as np
 np_elementwise = __import__('12-bracin_the_elements').np_elementwise
+import numpy as np
 def np_elementwise(mat1, mat2):
     return np.add(mat1,mat2),np.subtract(mat1,mat2),np.multiply(mat1,mat2),np.divide(mat1,mat2)
 mat1 = np.array([[11, 22, 33], [44, 55, 66]])
@@ -14,33 +13,4 @@ add, sub, mul, div = np_elementwise(mat1, mat2)
 print("Add:\n", add, "\nSub:\n", sub, "\nMul:\n", mul, "\nDiv:\n", div)
 add, sub, mul, div = np_elementwise(mat1, 2)
 print("Add:\n", add, "\nSub:\n", sub, "\nMul:\n", mul, "\nDiv:\n", div)
-alexa@ubuntu-xenial:0x00-linear_algebra$ ./12-main.py 
-[[11 22 33]
- [44 55 66]]
-[[1 2 3]
- [4 5 6]]
-Add:
- [[12 24 36]
- [48 60 72]] 
-Sub:
- [[10 20 30]
- [40 50 60]] 
-Mul:
- [[ 11  44  99]
- [176 275 396]] 
-Div:
- [[11. 11. 11.]
- [11. 11. 11.]]
-Add:
- [[13 24 35]
- [46 57 68]] 
-Sub:
- [[ 9 20 31]
- [42 53 64]] 
-Mul:
- [[ 22  44  66]
- [ 88 110 132]] 
-Div:
- [[ 5.5 11.  16.5]
- [22.  27.5 33. ]]
-alexa@ubuntu-xenial:0x00-linear_algebra$ 
+
