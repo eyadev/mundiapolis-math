0d5fa69b4b7128efb6ca6eb1dd42ac3e5a48ad27
eyadev <46867140+eyadev@users.noreply.github.com>
2021-03-11 20:52:03 +0100

    Update 2-size_me_please.py

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-import numpy as np 
 def matrix_shape(matrix):
-        return list (np.shape(matrix))
-
+    if type(matrix[0]) is not list:
+        return [len(matrix)]
+    else:
+        return [len(matrix)] + matrix_shape(matrix[0])
