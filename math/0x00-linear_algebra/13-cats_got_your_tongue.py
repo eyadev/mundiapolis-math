e4370520236e4ec84a1af27b68572f531f98bfe6
eyadev <46867140+eyadev@users.noreply.github.com>
2021-03-18 22:35:28 +0100

    Update 13-cats_got_your_tongue.py

--- math/0x00-linear_algebra/13-cats_got_your_tongue.py
+++ math/0x00-linear_algebra/13-cats_got_your_tongue.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-
-import numpy as np
+'''
+concatenate 2 matrices with numpy
+'''
 def np_cat(mat1, mat2, axis=0):
     if (len(mat1[0]) == len(mat2[0])) and axis == 0:
         return np.concatenate((mat1,mat2),axis=0)
     elif (len(mat1) == len(mat2)) and axis == 1:
         return np.concatenate((mat1,mat2),axis=1)
     else:
-        return None
-
+        return "None"
