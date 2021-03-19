aecb4f8b93041854c4d540ed21ffabeb131ed9a1
eyadev <46867140+eyadev@users.noreply.github.com>
2021-03-19 08:47:36 +0100

    Update 3-flip_me_over.py

--- math/0x00-linear_algebra/3-flip_me_over.py
+++ math/0x00-linear_algebra/3-flip_me_over.py
@@ -2,6 +2,8 @@
 '''
 Function that takes a matrix and return its transpose
 '''
+
+
 def matrix_transpose(matrix):
     trans = [[matrix[j][i] for j in range(len(matrix))] for i in range(len(matrix[0]))] 
     return trans
